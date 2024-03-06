94ac9fc76d6c1a5ee9328780062ebafd76e9603c
Isaac <isrodabough@gmail.com>
2024-03-06 08:45:52 -0700

    I am commiting because I feel like it. I have only edited my physics file.

--- collisions and physics.py
+++ collisions and physics.py
@@ -9,9 +9,22 @@ py.font.init()
 
 # class Track(self, )
 
-class Car(py.sprite.Sprite):
-    def __init__(self, rotation_V, max_V, starting_pos_x, starting_pos_y, img):
+# class Car(py.sprite.Sprite):
+#     def __init__(self, rotation_V, max_V, starting_pos_x, starting_pos_y, img):
+#         py.sprite.Sprite.__init__(self)
+#         self.image = img
+#         # self.turning_center = None
+#         # self.center_of_mass = 
+class PhysicsObject(py.sprite.Sprite):
+    def __init__(self, image: py.Surface, center_of_mass: tuple = None, density: float = 1):
         py.sprite.Sprite.__init__(self)
-        self.image = img
-        # self.turning_center = None
-        # self.center_of_mass = 
+        self.image = image
+        self.mask = py.mask.from_surface(self.image)
+        self.density = density
+        self.mass = self.density * self.mask.count()
+        self.center_of_mass = center_of_mass
+        self.x, self.y, self.dx, self.dy = 0, 0, 0, 0
+        self.angle, self.angular_velocity = 0, 0
+
+# class Car(PhysicsObject):
+    # def __init__(self, *)
